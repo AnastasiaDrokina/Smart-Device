4e02ff5bf862b93bfc8c523127cd49de46955ac7
AnastasiaDrokina <46569895+AnastasiaDrokina@users.noreply.github.com>
2019-12-11 19:22:36 +1000

    Добавляет модальное окно

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,42 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+var modalContacts = document.querySelector('.modal.contacts');
+var contactsToggle = document.querySelector('.header__btn');
+var modalCloseBtn = document.querySelector('.modal__close-btn');
+var overlay = document.querySelector('.overlay');
+var nameField = document.querySelector('.modal .contacts__name input');
 
-pageHeader.classList.remove('page-header--nojs');
+// Когда модальное окно закрыто
+contactsToggle.addEventListener('click', function () {
+  if (modalContacts.classList.contains('modal--close')) {
+    modalContacts.classList.remove('modal--close');
+    overlay.classList.remove('overlay--close');
+    nameField.focus();
+  }
+});
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+// Когда модальное окно открыто
+modalCloseBtn.addEventListener('click', function () {
+  if (!modalContacts.classList.contains('modal--close')) {
+    modalContacts.classList.add('modal--close');
+    overlay.classList.add('overlay--close');
+  }
+});
+
+// Закрытие клавишей ESC
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (!modalContacts.classList.contains('modal--close')) {
+      modalContacts.classList.add('modal--close');
+      overlay.classList.add('overlay--close');
+    }
+  }
+});
+
+// Закрытие overlay
+overlay.addEventListener("click", function () {
+  if (!modalContacts.classList.contains('modal--close')) {
+    modalContacts.classList.add('modal--close');
+    overlay.classList.add('overlay--close');
   }
 });
