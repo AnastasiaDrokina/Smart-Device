fa41bf31f21d4641215a6e315f63c68c4b6dacf8
AnastasiaDrokina <46569895+AnastasiaDrokina@users.noreply.github.com>
2019-12-12 23:19:36 +1000

    Добавляет аккордеон в футор, правит форму

--- build/js/script.js
+++ build/js/script.js
@@ -4,6 +4,10 @@ var contactsToggle = document.querySelector('.header__btn');
 var modalCloseBtn = document.querySelector('.modal__close-btn');
 var overlay = document.querySelector('.overlay');
 var nameField = document.querySelector('.modal .contacts__name input');
+var listServices = document.querySelector('.column__services .footer__list');
+var listContacts = document.querySelector('.column__contacts .footer__list');
+var btnServices = document.querySelector('.column__services .footer__btn');
+var btnContacts = document.querySelector('.column__contacts .footer__btn');
 
 // Когда модальное окно закрыто
 contactsToggle.addEventListener('click', function () {
@@ -38,5 +42,33 @@ overlay.addEventListener("click", function () {
   if (!modalContacts.classList.contains('modal--close')) {
     modalContacts.classList.add('modal--close');
     overlay.classList.add('overlay--close');
+  }
+});
+
+// Открывает и закрывает "Разделы сайта"
+btnServices.addEventListener('click', function () {
+  if (btnServices.classList.contains('footer__btn--opened')) {
+    btnServices.classList.remove('footer__btn--opened');
+  } else {
+    btnServices.classList.add('footer__btn--opened');
+  }
+  if (listServices.classList.contains('footer__list--opened')) {
+    listServices.classList.remove('footer__list--opened');
+  } else {
+    listServices.classList.add('footer__list--opened');
+  }
+});
+
+// Открывает и закрывает "Наш офис"
+btnContacts.addEventListener('click', function () {
+  if (btnContacts.classList.contains('footer__btn--opened')) {
+    btnContacts.classList.remove('footer__btn--opened');
+  } else {
+    btnContacts.classList.add('footer__btn--opened');
+  }
+  if (listContacts.classList.contains('footer__list--opened')) {
+    listContacts.classList.remove('footer__list--opened');
+  } else {
+    listContacts.classList.add('footer__list--opened');
   }
 });
